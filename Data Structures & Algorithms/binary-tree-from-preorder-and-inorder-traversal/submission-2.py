3152666398b1c588ca60c86e9ed9f4bce085484d
Alexander Alestra <58407966+AlexAlestra100@users.noreply.github.com>
2026-06-29 12:20:47 -0500

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-2

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-2.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        inOrder = {val: idx for idx, val in enumerate(inorder)}
+
+        preIdx = 0
+
+        def dfs(l, r):
+            nonlocal preIdx
+            if l > r:
+                return None
+
+            pOrdVal = preorder[preIdx]
+            preIdx += 1
+            curr = TreeNode(pOrdVal)
+            mid = inOrder[pOrdVal]
+
+            curr.left = dfs(l, mid - 1)
+            curr.right = dfs(mid + 1, r)
+
+            return curr
+
+        return dfs(0, len(preorder) - 1)
